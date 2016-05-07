991441eb1b370e047c15040bd8316bed89c28e5b
sarahkleins <kleinman.sarah@gmail.com>
2016-05-07 14:17:19 -0400

    add http_collect_async.js

---
+++ http_collect_async.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var urls = process.argv.slice(2);
+
+urls.forEach(function(url) {
+  var output = '';
+  http.get(url, function(response){
+    response.on('data', function(data){
+      output += data;
+    });
+    response.on('error', function(err){
+      return console.error('An errror occured: ' + err);
+    })
+    response.on('end', function(){
+      console.log(output);
+    });
+  });
+});
+
+
+
